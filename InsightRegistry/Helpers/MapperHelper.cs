18f8d4c5a61c06ff7c215aed3c6b488ec4c95fc6
Tsepo Motswaine <tsepo@mgibagroup.com>
2018-09-06 21:13:29 +0200

    Base structure

---
+++ InsightRegistry/Helpers/MapperHelper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace InsightRegistry.Helpers
+{
+    public class MapperHelper
+    {
+        internal static T Map<T, U>(T destination, U source) where T : class where U : class
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<U, T>());
+
+            var mapper = config.CreateMapper();
+
+            var returnModel = mapper.Map<U, T>(source);
+
+            return returnModel;
+        }
+    }
+}
